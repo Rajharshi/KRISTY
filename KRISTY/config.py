8a2e720be3758b262483cbf515d11260e02bc7f5
Rajharshi <124248443+Rajharshi@users.noreply.github.com>
2023-02-02 15:01:52 +0530

    Update config.py

--- KRISTY/config.py
+++ KRISTY/config.py
@@ -12,23 +12,23 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
      
-    API_ID = 123456  # integer value, dont use "" this sign get it form my.telegram.org
-    API_HASH = "" # get it form my.telegram.org
-    TOKEN = ""  # get it form @botfather.
-    OWNER_ID = 1820525265  # got to @miss_kristy_bot and type /id
+    API_ID = 15418866  # integer value, dont use "" this sign get it form my.telegram.org
+    API_HASH = "dbbf679a4b429fab1bfea0b52b8f9be8" # get it form my.telegram.org
+    TOKEN = "6091958748:AAEcw9e0jdy381xxQVeWUuV1eCoDSE7C3dA"  # get it form @botfather.
+    OWNER_ID = 5897579715 # got to @miss_kristy_bot and type /id
     OWNER_USERNAME = "I_AM_PRO_KING" # your telegram username
     ALLOW_CHATS = True # leave it as it is
-    BOT_USERNAME = "MISS_KRISTY_BOT" # your bot username get it form @botfather
-    SUPPORT_CHAT = "KRISTY_AF"  # Your own group for support, do not add the @ if you dont have leave it as it is
-    UPDATES_CHANNEL = "KRISTY_MF"  # Your own chsnnel for support, do not add the @ if you dont have leave it as it is
+    BOT_USERNAME = "Harshop12_musicbot" # your bot username get it form @botfather
+    SUPPORT_CHAT = "chatting_gruap"  # Your own group for support, do not add the @ if you dont have leave it as it is
+    UPDATES_CHANNEL = "chatting_gruap"  # Your own chsnnel for support, do not add the @ if you dont have leave it as it is
     JOIN_LOGGER =  (
-        -1001690301851
+        -1001618068091
     )  # add @miss_kristy_bot in your group and type /id
     EVENT_LOGS = (
-        -1001690301851
+        -1001618068091
     )  # add @miss_kristy_bot in your group and type /id
     ERROR_LOG = (
-        -1001690301851
+        -1001618068091
     )  # add @miss_kristy_bot in your group and type /id
     STRICT_GMUTE = True #to allow gmutes
     START_STICKER = "" #sticker id for start animation
@@ -38,8 +38,8 @@ class Config(object):
 
 
     # RECOMMENDED
-    STRING_SESSION ="" #telethon string session of user or bot get it from https://replit.com/@MISSKRISTY/MISS-KRISTY
-    MONGO_DB_URI = "" #get it from mongodb.com get
+    STRING_SESSION ="BQAoAyn21syI26TYs0_0H7NMU2P0kt0vZ7dxCCqv83z3NcDULz9ETFVax4Wl5hQMWlnezAY7JTrjGuXCM6blSy2xIWkEH3ISDY4x_sr7Ot_TCy2AY8fp3wyPnSKN_v3ZCPKTcJt4bdMoqsypqWove9aG4yAl4defkzFLYSkosZW3FJBYzQgeoYJkGQMR4IPiYXUwNHdwQv1XpuXUeJcMJGm9Z3PI5WQ6m0BJ8-YNMgqKK6P_QZxcbF_B4SzrvC0_0M4SBJQkvnNSwSe7r08dE5c8YaXiRyVvc95_lJ4FYLiMlApv5pfMT6UNnotTaWpJ1YVK4Z2UIKxW9QOD24IaaYpkAAAAAV-F7MMA" #telethon string session of user or bot get it from https://replit.com/@MISSKRISTY/MISS-KRISTY
+    MONGO_DB_URI = "mongodb+srv://vcbot:vcbot@cluster0.yqipgxg.mongodb.net/?retryWrites=true&w=majority" #get it from mongodb.com get
     ARQ_API_KEY = "" #git it form @ARQRobot
     ARQ_API_URL = "https://arq.hamker.in" # dont change
     SQLALCHEMY_DATABASE_URL = ""  # needed for any database modules get it from https://www.elephantsql.com/
@@ -48,8 +48,8 @@ class Config(object):
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
-    SPAMWATCH_SUPPORT_CHAT = "@KRISTY_AF"
+    SPAMWATCH_API = "chatting_gruap"  # go to support.spamwat.ch to get key
+    SPAMWATCH_SUPPORT_CHAT = "@"
 
 
     # OPTIONAL
@@ -62,7 +62,7 @@ class Config(object):
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     TIGERS = [1820525265]
     WOLVES = [1820525265]
-    START_IMG = "https://te.legra.ph/file/4c19a38ab1e2ab5e94b67.jpg" #yor fav img link
+    START_IMG = "https://te.legra.ph/file/963350097d10d22508deb.jpg" #yor fav img link
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
